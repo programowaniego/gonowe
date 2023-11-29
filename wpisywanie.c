45afd086a2fe19469e70cb77ea02e75b14884f18
programowaniego <151568070+programowaniego@users.noreply.github.com>
2023-11-29 22:43:29 +0100

    Update wpisywanie.c

--- wpisywanie.c
+++ wpisywanie.c
@@ -1,16 +1,16 @@
 #ifndef wpisywanie.c
-int pozycja(char gracz[2])
+int pozycja(plansza* stol)
 {
 int pozycja = 0;
  char kolumna;
  int wiersz;
-printf("Tura %s \nPodaj pole na ktorym chcesz postawic pionek (kolumna , wiersz)(np A1)\n",gracz);  
+printf("Tura %s \nPodaj pole na ktorym chcesz postawic pionek (kolumna , wiersz)(np A1)\n",stol->gracz_na_ruchu);  
 scanf("%c%d", &kolumna , &wiersz);
 getchar();
  while(kolumna < 'A'|| kolumna  > 'A' + kolumny || wiersz < 0 || wiersz > wiersze || wiersz == ' ' || kolumna == ' ')
     {
         printf("podano niewlasciwa wartosc kolumny/wiersza\n");
-        printf("Tura %s \n Podaj pole na ktorym chcesz postawic pionek (kolumna , wiersz)(np A1)\n",gracz);  
+        printf("Tura %s \n Podaj pole na ktorym chcesz postawic pionek (kolumna , wiersz)(np A1)\n",stol->gracz_na_ruchu);  
         scanf("%c%d", &kolumna , &wiersz);
         getchar();
     }
