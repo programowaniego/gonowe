b20422dfa6253bb0028c95da3291bdd20c17239f
programowaniego <151568070+programowaniego@users.noreply.github.com>
2023-11-29 23:25:01 +0100

    Scanf o niego lepiej działa

--- wpisywanie.c
+++ wpisywanie.c
@@ -3,10 +3,13 @@ int pozycja(plansza* stol)
 {
  char kolumna;
  int wiersz;
-printf("Tura %s \nPodaj pole na ktorym chcesz postawic pionek (kolumna , wiersz)(np A1)\n",stol->gracz_na_ruchu);  
-scanf("%c%d", &kolumna , &wiersz);
-getchar();
- while(kolumna < 'A'|| kolumna  > 'A' + kolumny || wiersz < 0 || wiersz > wiersze || wiersz == ' ' || kolumna == ' ')
+int znaki = 0;
+do {
+        printf("Tura %s \nPodaj pole na ktorym chcesz postawic pionek (kolumna , wiersz)(np A1)\n",stol->gracz_na_ruchu);  
+        int wejscie = scanf(" %c%d", &kolumna, &wiersz);
+        znaki = getchar();
+    } while (znaki == " ");
+while(kolumna < 'A'|| kolumna  > 'A' + kolumny || wiersz < 0 || wiersz > wiersze)
     {
         printf("podano niewlasciwa wartosc kolumny/wiersza\n");
         printf("Tura %s \n Podaj pole na ktorym chcesz postawic pionek (kolumna , wiersz)(np A1)\n",stol->gracz_na_ruchu);  
