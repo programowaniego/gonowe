b3ba27e53de335ee622a61ae5586daac86f6eaac
programowaniego <151568070+programowaniego@users.noreply.github.com>
2023-11-29 17:44:00 +0100

    Update głowny plik.c

--- głowny plik.c
+++ głowny plik.c
@@ -1,1 +1,18 @@
-
+#include<stdio.h>
+#include<stdlib.h>
+#include<malloc.h>
+#include "Plansza.c"
+#include "wpisywanie.c"
+#include "wykonywanie_ruchow.c"
+void main()
+{
+    plansza* stol = utworz_plansze(stol);
+    for(int i = 1;i <= wiersze*kolumny;i++){
+        postaw_pionek(stol,pozycja(tura(i)),tura(i));//warunek na puste pole
+        ruchy* mozliwe = mozliwe_ruchy(mozliwe,stol);
+        //printf("%d\n",mozliwe->dostepne_ruchy[169]);
+        free(mozliwe); 
+    }
+    
+}
+    
