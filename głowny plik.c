efd8324261f058d41968a75d6a92faa38ae80ebd
programowaniego <151568070+programowaniego@users.noreply.github.com>
2023-12-06 11:41:07 +0100

    Update głowny plik.c

--- głowny plik.c
+++ głowny plik.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<malloc.h>
-#include <windows.h>
+#include<windows.h>
 #include "Plansza.c"
 #include "wpisywanie.c"
 #include "wykonywanie_ruchow.c"
@@ -21,19 +21,19 @@ void main()
     }
     for(int i = 1;i <= wiersze*kolumny && 1;i++){
         int polozenie = pozycja(stol);
-        
+
         if(polozenie != 2137){
             postaw_pionek(stol,polozenie);//warunek na puste pole
-        
+
         if(stol->gracz_na_ruchu == "O")
                 stol->gracz_na_ruchu = "X";
-            
+
         else
                 stol->gracz_na_ruchu = "O";
         }
-        
+
     }
-    
+
         //ruchy* mozliwe = mozliwe_ruchy(mozliwe,stol);
         //printf("%d\n",mozliwe->dostepne_ruchy[169]);
         //free(mozliwe); 
