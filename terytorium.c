821e9d46af5053e3c818cec7cdbe4a35727ab975
programowaniego <151568070+programowaniego@users.noreply.github.com>
2023-12-06 16:56:24 +0100

    Update terytorium.c

--- terytorium.c
+++ terytorium.c
@@ -5,23 +5,24 @@ int terytorium(plansza* stol,int pole,int moje_pole[],char *napotkane){
     int dodatki[] = {1,-1,kolumny,-kolumny};
     moje_pole[pole] = 1;
     stol->wartosci[pole] = "1";
-    
     for(int i = 0;i < 4;i++){
         if(pole + dodatki[i] >= 0 && pole + dodatki[i] < wiersze * kolumny && moje_pole[pole + dodatki[i]] == 0){     //nie wychodzimy poza planszę
             
             if((pole + dodatki[i] - (pole + dodatki[i]) % kolumny)/kolumny == (pole - pole % kolumny)/kolumny || (pole + dodatki[i])% kolumny == pole % kolumny){ //zapobiegamy zmianie kolumny i wiersza na raz
                 
-                if(stol->wartosci[pole + dodatki[i]] != " "){
-
-                    if(*stol->wartosci[pole + dodatki[i]] != *napotkane && stol->wartosci[pole + dodatki[i]] != "1"){  //sprawdzamy czy funkcja już napotkała taki pionek
-                        *napotkane = *stol->wartosci[pole + dodatki[i]];
-                        printf("%d %c\n",pole + dodatki[i],*napotkane);
-                    }
-                }
-                if(stol->wartosci[pole + dodatki[i]] == " "){ //funkcja idzie dalej kiedy spotka puste pole
-                    
+                if(stol->wartosci[pole + dodatki[i]] == " " && terytorium(stol, pole + dodatki[i],moje_pole,napotkane) == 0){ //funkcja idzie dalej kiedy spotka puste pole
                     terytorium(stol, pole + dodatki[i],moje_pole,napotkane);
-                }    
+                    return 0;
+                }
+                if(stol->wartosci[pole + dodatki[i]] != " "){
+                    if(*napotkane == NULL){
+                        *napotkane = *stol->wartosci[pole + dodatki[i]];
+                        printf("%d %c",pole + dodatki[i],*napotkane);
+                    }
+                    if(*stol->wartosci[pole + dodatki[i]] != *napotkane && stol->wartosci[pole + dodatki[i]] != "1"){  //sprawdzamy czy funkcja już napotkała taki pionek
+                        return 0;
+                    }
+                } 
             }
         }
     }
@@ -34,6 +35,16 @@ int ile_pol(plansza* stol,int wejscie[],int czy_pionki){     //funkcja zliczają
         else if(wejscie[i] && czy_pionki)
             k++;
     }
+    return k;
+}
+int ile_pionkow(plansza* stol, char* gracz)
+{
+    int k = 0;
+        for(int i = 0 ; i < wiersze * kolumny ; i++ ){
+            if(*stol->wartosci[i] == *gracz) 
+            k++; 
+        
+        }
     return k;
 }
 #endif
