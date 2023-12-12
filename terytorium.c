2f247a8a5e843d16946800e114f31f20ba74a98b
programowaniego <151568070+programowaniego@users.noreply.github.com>
2023-12-12 21:17:33 +0100

    Update terytorium.c

--- terytorium.c
+++ terytorium.c
@@ -11,7 +11,7 @@ int terytorium(plansza* stol,int pole,int moje_pole[],char *napotkane){
     int dodatki[] = {1,-1,kolumny,-kolumny};
     moje_pole[pole] = 1;
     stol->wartosci[pole] = "1";
-    
+     
     for(int i = 0;i < 4;i++){
         if(pole + dodatki[i] >= 0 && pole + dodatki[i] < wiersze * kolumny && moje_pole[pole + dodatki[i]] == 0){     //nie wychodzimy poza planszę
             
@@ -20,8 +20,8 @@ int terytorium(plansza* stol,int pole,int moje_pole[],char *napotkane){
                 if(stol->wartosci[pole + dodatki[i]] != " " && stol->wartosci[pole + dodatki[i]] != "1"){
                     
                     if(*stol->wartosci[pole + dodatki[i]] != napotkane[dlugosc(napotkane) - 1] && dlugosc(napotkane) < 2){  //sprawdzamy czy funkcja już napotkała taki pionek
-                            napotkane[dlugosc(napotkane)] = *stol->wartosci[pole + dodatki[i]];
-                            printf("%d %c\n",pole + dodatki[i],napotkane[dlugosc(napotkane)]);
+                        napotkane[dlugosc(napotkane)] = *stol->wartosci[pole + dodatki[i]];
+                        printf("%d %c\n",pole + dodatki[i],napotkane[dlugosc(napotkane) - 1]);
                     }
                 }
                 if(stol->wartosci[pole + dodatki[i]] == " "){ //funkcja idzie dalej kiedy spotka puste pole
@@ -32,12 +32,21 @@ int terytorium(plansza* stol,int pole,int moje_pole[],char *napotkane){
         }
     }
 }
-int ile_pol(plansza* stol){     //funkcja zliczająca pola terytorium w oparciu o jedynki
+int ile_pol(plansza* stol,int wejscie[]){     //funkcja zliczająca pola terytorium w oparciu o listę moje_pole
     int k = 0;
     for(int i = 0;i < wiersze * kolumny;i++){
         if(stol->wartosci[i] == "1")
             k++;
     }
+    return k;
+}
+int ile_pionkow(plansza* stol, char* gracz)
+{
+    int k = 0;
+        for(int i = 0 ; i < wiersze * kolumny ; i++ )
+        {
+        if(stol->wartosci[i] == gracz) k++; 
+        }
     return k;
 }
 #endif
