6e1e57562cd9864b0716121fbfda29237001d545
programowaniego <151568070+programowaniego@users.noreply.github.com>
2023-12-12 21:19:45 +0100

    Update głowny plik.c

--- głowny plik.c
+++ głowny plik.c
@@ -6,40 +6,35 @@
 #include "wpisywanie.c"
 #include "wykonywanie_ruchow.c"
 #include "terytorium.c"
+#include "ocena.c"
 
 void main()
 {
-    plansza* stol = utworz_plansze(stol);
-    //int pozycjeX[] = {3,4,11,12,21,30,29,28,27,36,38,40,49,50,51,58,56,60,61,65,67,69,71,77,77,77}; inna pozycja
-    //int pozycjeY[] = {5,13,14,15,22,31,33,34,37,39,41,42,44,45,46,47,48,52,53,55,57,62,64,66,75,76};
-    int pozycjeX[] = {3,4,5,11,13,21,30,57,67,68,75};
-    int pozycjeY[] = {6,15,14,23,22,31,58,59,61,69,78};
-    for(int i = 0;i < 11;i++){
+    plansza* stol = utworz_plansze(stol,"X");
+    int pozycjeX[] = {3,4,11,12,21,30,29,28,27,36,38,40,49,50,51,58,56,60,61,65,67,69,71,77,77,77};
+    int pozycjeY[] = {5,13,14,15,22,31,33,34,37,39,41,42,44,45,46,47,48,52,53,55,57,62,64,66,75,76};
+    //int pozycjeX[] = {3,4,5,11,13,21,30,57,67,68,75};
+    //int pozycjeY[] = {6,15,14,23,22,31,58,59,61,69,78};
+    for(int i = 0;i < 26;i++){
         postaw_pionek(stol,pozycjeX[i]);
         stol->gracz_na_ruchu = "O";
         postaw_pionek(stol,pozycjeY[i]);
         stol->gracz_na_ruchu = "X";
 
     }
+    //postaw_pionek(stol,0);
+    stol->gracz_na_ruchu = "O";
     wypisz(stol);
-    //element* poczatek = kolejne_dostepne(stol);
-    element* glowa = NULL;
-    //glowa = utworz_element(glowa , (ruchy) { .pozycja = 1});
-    //kolejne_dostepne(stol,glowa);
-    for(int i = 0;i < wiersze * kolumny;i++)
-    {
-        if(stol->wartosci[i] == " ")
-        {
-            glowa = utworz_element(glowa , (ruchy){ .pozycja = i});
-        }
-    }
+    ocena_pozycji(stol);
+    /*
+    element* glowa = dostepne_ruchy(stol,glowa);
     int k = dlugosc_jedno(glowa);
     for(int i = 0;i < k;i++){
         
-        printf("%d\n",glowa->dostepne);
+        printf("%c%d\n",glowa->dostepne.pozycja % kolumny + 'A',1 + (glowa->dostepne.pozycja - glowa->dostepne.pozycja % kolumny)/kolumny);
         glowa = glowa->nastepny;
     }
-    
+    */
     for(int i = 1;i <= wiersze*kolumny && 1;i++){
         int polozenie = pozycja(stol);
 
@@ -55,10 +50,6 @@ void main()
         }
 
     }
-
-        //ruchy* mozliwe = mozliwe_ruchy(mozliwe,stol);
-        //printf("%d\n",mozliwe->dostepne_ruchy[169]);
-        //free(mozliwe); 
 }
     
     
